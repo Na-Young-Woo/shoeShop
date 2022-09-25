29ca37a8dd4f46db1f09de1b128850561f749f76
nywoo <dn2255520@gmail.com>
2022-09-25 12:43:57 +0900

    component 추가 시작(home 컴포넌트 추가)

---
+++ src/components/home/CalltoActionSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CalltoActionSection = () => {
+  return (
+    <div>
+      <div className="subscrib-section bg-with-black">
+        <div className="container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CalltoActionSection;

b3e209df15efd7da95b669a1f54b6ff1bf7fb560
nywoo <dn2255520@gmail.com>
2022-09-26 20:17:50 +0900

    CalltoActionSection JSX 작성

--- src/components/homeComponents/CalltoActionSection.js
+++ src/components/homeComponents/CalltoActionSection.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const CalltoActionSection = () => {
   return (
-    <div>
-      <div className="subscrib-section bg-with-black">
-        <div className="container">test</div>
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="subscribe-head">
+              <h2>DO you need more tips?</h2>
+              <p>Sign up free and get the latest tips.</p>
+              <form className="form-section">
+                <input placeholder="Your Email..." name="email" type="email" />
+                <input value="Yes. I want!" name="subscribe" type="submit" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

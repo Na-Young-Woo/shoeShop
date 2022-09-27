a1dab7a83affe755061647cba866f78a200c0af2
nywoo <dn2255520@gmail.com>
2022-09-27 09:25:20 +0900

    homeScreen 컴포넌트 추가 및 페이지 로드

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>Header입니다</div>
+    </div>
+  );
+};
+
+export default Header;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
 import CalltoActionSection from "../components/homeComponents/CalltoActionSection";
+import ContactInfo from "../components/homeComponents/ContactInfo";
+import Footer from "../Footer";
+import Header from "../Header";
 
 const HomeScreen = () => {
   return (
     <div>
+      <Header />
       <CalltoActionSection />
+      <ContactInfo />
+      <Footer />
     </div>
   );
 };

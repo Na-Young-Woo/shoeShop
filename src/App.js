566f8d0704236b4ac48cec2c5e3e660530f440be
nywoo <dn2255520@gmail.com>
2022-10-04 23:48:39 +0900

    Register screen 추가 및 App.js에 Route 연결

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/Login";
 import ProfileScreen from "./screens/ProfileScreen";
+import Register from "./screens/Register";
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
         <Route path="/" element={<HomeScreen />} exact />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<ProfileScreen />} />
+
+        <Route path="register" element={<Register />} />
       </Routes>
     </Router>
   );

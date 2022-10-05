3fa1ef1758db95e52235172201b92afaffdf2de5
nywoo <dn2255520@gmail.com>
2022-10-05 10:17:36 +0900

    Register 추가

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 const Register = () => {
@@ -6,6 +7,19 @@ const Register = () => {
   return (
     <div>
       <Header />
+      <div className="container d-flex flex-column justify-content-center align-item-center login-center">
+        <form action="" className="Login col-md-4 col-lg-8 col-11">
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Password" />
+          <button>submit</button>
+          <p>
+            <Link>
+              I Have Account <strong>Login</strong>
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };

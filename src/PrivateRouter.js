2e5fe1ce2e88b725be2f426a3e0158d06bb8a4a5
nywoo <dn2255520@gmail.com>
2022-10-09 21:25:18 +0900

    PrivateRouter 추가

---
+++ src/PrivateRouter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Redirect, Route } from "react-router";
+("react-router-dom");
+
+function PrivateRouter() {
+  <Route Redirect={"/login"} />;
+}
+export default PrivateRouter;

747fb6394b8fa8c2b6bc0fe91f1c875a9738eacf
nywoo <dn2255520@gmail.com>
2022-10-15 09:39:55 +0900

    store 수정 중..

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,13 @@
 // https://velog.io/@anotherhoon/createStore%EB%A5%BC-configureStore%EB%A1%9C-%EB%B0%94%EA%BE%B8%EA%B8%B0
 //
-import { combineReducers, createStore } from "redux";
+// import { combineReducers, createStore } from "redux";
 
-const reducer = combineReducers;
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+const reducers = {
+  userLogin: "",
+};
 const userInfoFromLocalStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : {};
@@ -10,7 +15,8 @@ const userInfoFromLocalStorage = localStorage.getItem("userInfo")
 const initialState = {
   cart: { userLogin: { userInfo: userInfoFromLocalStorage } },
 };
-
-const store = createStore();
+// const middleware = [thunk];
+// 유효한 리듀서 넣어라..
+const store = configureStore({ reducer: {} });
 
 export default store;
